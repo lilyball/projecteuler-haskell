ea96bc9846031be1cff9fc4c2995cccffc679100
Kevin Ballard <kevin@sb.org>
2008-10-09 06:19:36 -0700

    Add solution for euler3

---
+++ euler3.hs
@@ -0,0 +1,12 @@
+{-
+http://projecteuler.net/index.php?section=problems&id=3
+
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+-}
+
+import Utilities
+
+main = do
+    print $ last $ primeFactors 600851475143
