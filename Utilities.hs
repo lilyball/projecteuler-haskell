9ae6b62926113ac630728142e273fe01b28c664a
Kevin Ballard <kevin@sb.org>
2008-10-09 23:15:36 -0700

    Implement divisors function in Utilities
    Also add an error message to primeFactors for invalid input

--- Utilities.hs
+++ Utilities.hs
@@ -5,12 +5,15 @@ A collection of miscellaneous utilities used for project euler solutions
 module Utilities
     (
       primeFactors
+    , divisors
     ) where
 
 import qualified Sieve
 import Data.List
+import qualified Data.Set as Set
 
 primeFactors :: Integer -> [Integer]
+primeFactors n | n <= 0 = error "primeFactors: n must be >= 1"
 primeFactors n =
     unfoldr aux (n, Sieve.primes)
   where aux :: (Integer, [Integer]) -> Maybe (Integer, (Integer, [Integer]))
@@ -18,3 +21,8 @@ primeFactors n =
         aux (n, pps@(p:ps))
             | n `mod` p == 0 = Just (p, (n `div` p, pps))
             | otherwise      = aux (n, ps)
+
+divisors n = Set.toList $ divisors' $ primeFactors n
+  where divisors' []     = Set.singleton 1
+        divisors' (n:ns) = subdivs `Set.union` Set.map (n*) subdivs
+          where subdivs = divisors' ns
