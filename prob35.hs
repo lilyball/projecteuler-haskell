e0ee92f91972f669faf18dc0dbfd387b72d16c33
Kevin Ballard <kevin@sb.org>
2011-12-15 19:26:37 -0800

    Add prob35.hs

---
+++ prob35.hs
@@ -0,0 +1,32 @@
+-- The number, 197, is called a circular prime because all rotations of the
+-- digits: 197, 971, and 719, are themselves prime.
+--
+-- There are thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 31, 37, 71,
+-- 73, 79, and 97.
+--
+-- How many circular primes are there below one million?
+--
+
+import Data.Numbers.Primes
+import Data.Maybe
+import Data.Char
+
+rotations :: Int -> Maybe [Int]
+rotations n
+    | hasZero = Nothing
+    | otherwise = Just rots
+  where hasZero = any (== '0') digits
+        digits = show n
+        rots = map read rots'
+        rots' = take (length digits) $ iterate rot digits
+        rot (d:ds) = ds ++ [d]
+
+isCircularPrime :: Int -> Bool
+isCircularPrime n
+    | n == 2 = True -- everything past 2 cannot be even
+    | all odd digits = maybe False (all isPrime) $ rotations n
+    | otherwise = False
+  where digits = map digitToInt $ show n
+
+main = do
+  print $ length $ filter isCircularPrime $ [1..1000000]
